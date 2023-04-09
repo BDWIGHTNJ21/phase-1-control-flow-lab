b3f99ef385237926afbe65abccb747f32d4da115
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-04-09 01:27:25 -0400

    completed

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x) {
   // Write your code here!
+ 
+  if (x<= 400){
+    return `This one is on me!`; 
+
+   } else if  (x >400 && x <2000) {
+    return"That will be twenty bucks.";
+ } else if (x>2000 && x <2500){
+  return "I will gladly take your thirty bucks.";
+} else {
+  return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
+function ternaryCheckCity(z) {
+ 
+ return (z=="NYC"? "Ok, sounds good.": "No go.");
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
+function switchOnCharmFromTip(t){
+return (t=="generous"? "Thank you so much." : t== "not as generous"?"Thank you.": "Bye.");
+}
+// return "thank for everything"
